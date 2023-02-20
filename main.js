45f16b00444bab073facff17c8a9e99d3785c735
unknown <tel.salatiel2@gmail.com>
2023-02-20 16:25:22 -0300

    parte 5 eventos e lógica

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-function tocaSom(idElementoAudio) {
-  document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+  const elemento = document.querySelector(seletorAudio);
+
+  if (elemento && elemento.localName === 'audio') {
+      elemento.play();
+  }
+  else {
+    console.log('Elemento não encontrado ou seletor inválido');
+  }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -12,10 +19,20 @@ for(let contador = 0; contador < listaDeTeclas.length; contador++) {
 
   //template string
   const idAudio = `#som_${instrumento}`;
-  // console.log(idAudio);
 
   tecla.onclick = function() {
     tocaSom(idAudio);
   }
-  // console.log(contador);
+
+  tecla.onkeydown = function(evento) {
+
+    if (evento.code === 'Space' || evento.code === 'Enter') {
+      tecla.classList.add('ativa');
+    }
+    
+  }
+
+  tecla.onkeyup = function() {
+    tecla.classList.remove('ativa');
+  }
 }
